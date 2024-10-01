b5da31c99554899bd9b89662673cc8d5e3160634
Khirushan-Sasitharan-ICD2O1-01 <khirushan.sasitharan@student.tdsb.on.ca>
2024-10-01 13:50:59 +0000

    ddd

--- PP1_1.py
+++ PP1_1.py
@@ -1,19 +1,18 @@
 def q1():
-print ("Hello World")
+    print ("Hello World")
 
 def q2():
-  print ("1")
-  print ("2")
-  print ("3")
-  print ("4")
-  print ("5")
-  
+    print ("1\n2\n3\n4\n5")
+ 
 def q3():
- print (" Help Me!")
-def q4:
- print ("3 x 3 = 9")
+    print (" Help Me!")
+
+def q4():
+    print("3 x 3 = 9")
 
 q1()
 q2()
 q3()
 q4()
+
+
